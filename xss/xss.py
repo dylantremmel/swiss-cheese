b35aed5f35ec8a32a34966a1793aebae983ecfb3
dylantremmel <114194943+dylantremmel@users.noreply.github.com>
2024-02-15 23:19:31 -0500

    Update xss.py

--- xss/xss.py
+++ xss/xss.py
@@ -1,18 +1,30 @@
+#copy/pasted from my terminal where i was running it
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+# new function
+def contains_special_characters(input_string): 
+    special_chars = ['<', '>', '&', '"', "'", ';', '/', '\\']
+    for char in input_string:
+        if char in special_chars:
+            return True
+    return False
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     user_input = ''
     if request.method == 'POST':
-        # Retrieve user input from the form
         user_input = request.form.get('user_input', '')
 
-        # Display user input directly without proper sanitization (for demonstration purposes)
-        return render_template('index.html', user_input=user_input, display_script=True)
+        # Sanitization conditional
+        if contains_special_characters(user_input):
+            error_message = "Error: special characters found."
+            user_input = ''
+            return render_template('index.html', user_input=user_input, display_script=False, error_message=error_message)
+        else:
+            return render_template('index.html', user_input=user_input, display_script=True)
 
     return render_template('index.html', user_input=user_input, display_script=False)
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5001)
